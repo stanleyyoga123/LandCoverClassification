7e17fa626c1172e5f5d45bb70e75e4900e1af8c6
stanleyyoga123 <yoga360227@gmail.com>
2020-08-22 21:32:33 +0700

    fix bug for linux

--- src/classifier.py
+++ src/classifier.py
@@ -27,7 +27,12 @@ def run(path, cluster, save=True):
             images[el][i][j] = red
     
     if(save):
-        name = path.split('\\')[-1].split('.')[0]
+        # name = path.split('\\')[-1].split('.')[0]
+        name = path.split('\\')
+        if(len(name) == 1):
+            name = path.split('/')
+        
+        name = name[-1].split('.')[0]
         for i, image in enumerate(images):
             image = cv2.resize(image, (200,200))
             cv2.imwrite(os.path.join(IMAGES_FOLDER, f'{name}_{i}.png'), image)
